100c7abead4fab596c11131225a0ae31c668d7c2
Hayden Engeling <haydentoddengeling@gmail.com>
2021-08-22 18:38:43 -0500

    oatuh set up 1

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '226817307097-u0er9uc8etummibj9frptj1deijren8q.apps.googleusercontent.com',
+  googleClientSecert: 'd64FciviazDer1nMtHDiQ5jY',
+}
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there' });
+// });
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecert,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile token', profile);
+  }));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
