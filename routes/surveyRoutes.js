15cac8015a238d9c70e5c6f93d7adc70eb351ad7
Hayden Engeling <haydentoddengeling@gmail.com>
2021-09-01 20:38:12 -0500

    finished course work

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin.js');
 const checkCredits = require('../middlewares/checkCredits.js');
@@ -7,6 +10,51 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate.js');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
+
   app.post('/api/surveys', requireLogin, checkCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -19,7 +67,19 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
-    const mailer = new Mailer(survey, surveyTemplate(survey))
-    await mailer.send();
-  })
-}
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+
+    } catch (err) {
+      console.log(err);
+      res.status(422).send(err);
+    }
+  });
+};
--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
     super();
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email('haydentoddengeling@gmail.com');
+    this.from_email = new helper.Email('hengeling@gmail.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
@@ -59,28 +59,3 @@ class Mailer extends helper.Mail {
 }
 
 module.exports = Mailer;
-
-///////////////////////////////////////////////
-
-// const sgMail = require("@sendgrid/mail");
-// const keys = require("../config/keys");
-
-// class Mailer {
-//   constructor({ subject, recipients }, content) {
-//     sgMail.setApiKey(keys.sendGridKey);
-//     this.msg = {
-//       to: recipients.map(({ email }) => email),
-//       from: "haydentoddengeling@gmail.com",
-//       subject: subject,
-//       html: content,
-//       trackingSettings: { enable_text: true, enabled: true }
-//     };
-//   }
-
-//   async send() {
-//     const response = await sgMail.send(this.msg);
-//     return response;
-//   }
-// }
-
-// module.exports = Mailer;
